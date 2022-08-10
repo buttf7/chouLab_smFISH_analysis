13b6ecaf0837597a9140d3b3004f83fbeedb3154
buttf7 <103357482+buttf7@users.noreply.github.com>
2022-08-10 00:16:37 -0400

    Update read_img.py

--- fishSearch/read_img.py
+++ fishSearch/read_img.py
@@ -1,5 +1,18 @@
-#Function for reading image
-# Author: Arthur Imbert <arthur.imbert.pro@gmail.com>
+from bigfish import stack
+import os
+import bigfish
+from pathlib import Path
+from tqdm import tqdm
+import tkinter as tk
+from tkinter import filedialog
+import bigfish.plot as plot
+from cellpose import models
+from matplotlib import pyplot as plt
+from cellpose import plot as plt2
+import pandas as pd
+import numpy as np
+import bigfish.segmentation as segmentation
+
 def read_img():
     root = tk.Tk()
     root.withdraw()
