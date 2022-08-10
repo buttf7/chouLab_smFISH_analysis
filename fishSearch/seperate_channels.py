b226bcbb3b19b9a116da0b925283f401dafaae58
buttf7 <103357482+buttf7@users.noreply.github.com>
2022-08-10 00:17:08 -0400

    Update seperate_channels.py

--- fishSearch/seperate_channels.py
+++ fishSearch/seperate_channels.py
@@ -1,4 +1,19 @@
 #Function for creating seperate image channels
+from bigfish import stack
+import os
+import bigfish
+from pathlib import Path
+from tqdm import tqdm
+import tkinter as tk
+from tkinter import filedialog
+import bigfish.plot as plot
+from cellpose import models
+from matplotlib import pyplot as plt
+from cellpose import plot as plt2
+import pandas as pd
+import numpy as np
+import bigfish.segmentation as segmentation
+
 def seperate_channels(img, img_name):    
     #Sorted shape
     sorted_shape = sorted([i for i in img.shape])
