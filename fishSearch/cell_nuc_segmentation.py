10e58df5303002d9b74873ad68500dbda0f7b607
buttf7 <103357482+buttf7@users.noreply.github.com>
2022-08-10 00:14:16 -0400

    Update cell_nuc_segmentation.py

--- fishSearch/cell_nuc_segmentation.py
+++ fishSearch/cell_nuc_segmentation.py
@@ -1,3 +1,11 @@
+from bigfish import stack
+import os
+import bigfish
+from pathlib import Path
+from tqdm import tqdm
+import tkinter as tk
+from tkinter import filedialog
+import bigfish.plot as plot
 from cellpose import models
 from matplotlib import pyplot as plt
 from cellpose import plot as plt2
